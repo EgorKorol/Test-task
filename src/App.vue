eac258cd5d821e8ee6b76bda216294d835837a17
Egor Korol <egor.korol@mail.ru>
2018-07-01 19:53:53 +0300

    Added show/hide menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang='pug'>
 	#app
-		.wrapper
-			header-page.wrapper__header
-			navigation
+		.wrapper(:class="isShowMenu ? '' : 'wrapper--close-menu'")
+			header-page.wrapper__header(@toggleMenu='toggleMenu')
+			navigation.wrapper__aside(:class="isShowMenu ? '' : 'wrapper__aside--hidden'" )
 			content-page.wrapper__content
 </template>
 
@@ -13,6 +13,17 @@ export default {
 		HeaderPage: () => import('@/components/Header'),
 		Navigation: () => import('@/components/Navigation'),
 		ContentPage: () => import('@/components/Content')
+	},
+	data () {
+		return {
+			isShowMenu: true
+		}
+	},
+	methods: {
+		toggleMenu () {
+			console.log(111)
+			this.isShowMenu = !this.isShowMenu
+		}
 	}
 }
 </script>
@@ -21,7 +32,24 @@ export default {
 @import '@/assets/fonts/fonts.scss';
 @import '@/assets/base.scss';
 .wrapper__header, .wrapper__content {
-	width: calc(100% - 240px);
 	margin-left: auto;
+	width: calc(100% - 240px);
+	transition: .2s ease;
+}
+.wrapper__aside {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 240px;
+	height: 100%;
+	transition: .2s ease;
+	&--hidden {
+		transform: translateX(-100%);
+	}
+}
+.wrapper--close-menu {
+	.wrapper__header, .wrapper__content {
+		width: 100%;
+	}
 }
 </style>
